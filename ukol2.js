38aa3c02cce0d429e0a11db0dc27b5bb70dbbb1c
LaSi6 <lada.silhava@email.cz>
2022-01-22 14:03:34 +0100

    2 files modified

--- ukol2.js
+++ ukol2.js
@@ -1,5 +1,23 @@
-console.log(`Vaše jméno je ${jmeno}, je vám ${vek} let`)
-if (vek > podminka)
-  {console.log(' a tak máte právo řídit auto ')} 
-    else {consol.log(`a tak musíte počkat ještě ${podminka-vekOdpoved} roků, než budete moci řídit auto')}
-
+let jmeno = prompt('jak se jmenuješ?');
+let vek = Number(prompt('kolik je ti let?'));
+let podminka = 18;
+let doba = podminka - vek;
+console.log(`Vaše jméno je ${jmeno}, je vám ${vek} let,`);
+if (vek > podminka) {
+  console.log(' a tak máte právo řídit auto.');
+}
+if (doba === 1) {
+  console.log(
+    `a tak musíte počkat ještě ${doba} rok, než budete moci řídit auto.`
+  );
+}
+if (doba > 1 && doba < 5) {
+  console.log(
+    `a tak musíte počkat ještě ${doba} roky, než budete moci řídit auto.`
+  );
+}
+if (doba > 5) {
+  console.log(
+    `a tak musíte počkat ještě ${doba} let, než budete moci řídit auto.`
+  );
+}
